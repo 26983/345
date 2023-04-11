3b839f0be1edd4e7b578f79a9aea1d72a94e45ae
26983 <130429943+26983@users.noreply.github.com>
2023-04-11 15:17:08 +0900

    assignment2

---
+++ pages/posts/first-post.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+export default function FirstPost() {
+  return (
+    <>
+      <h1>My name is ZHENGNIYA</h1>
+      <p>My hobby is singing..</p>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </>
+  );
+}
+
